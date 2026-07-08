1edfb6e8b77c4a500a2638949dbf35b59a67faa2
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2026-07-08 12:02:06 +0100

    Initial commit

---
+++ VitaCheck.py
@@ -0,0 +1,8 @@
+# Input
+print("Enter your weight:")
+weight = input()
+print("Enter your height:")
+height = input()
+# Processing
+# Output
+print("You weigh: " + weight + " and you are " + height + " meters tall")
