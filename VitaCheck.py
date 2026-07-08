10f23676a7c6dfbe7f917463091dfb5146c4ad6d
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2026-07-08 14:50:44 +0100

    Input validation
    Added two while loops for height and weight
    Both contain try and except blocks

--- VitaCheck.py
+++ VitaCheck.py
@@ -1,9 +1,29 @@
-# Input
-print("Enter your weight in kilograms (0-200):")
-weight = input()
-print("Enter your height in meters (0 - 3):")
-height = input()
+# #Functions
+# def weight_input():
+#   try:
+#     weight = int(input("Please enter your weight in Kgs (between 0-200) "))
+#     type(weight) == int and weight >= 0 and weight <= 200
+#     return weight
+#   except: 
+#     print("Please enter a valid weight input (Must be a number and within the specified range)")
+#     weight_input()
+
+# Weight input validation
+while True: 
+  try:
+    weight = float(input("Enter your weight in kilograms (between 0-200) "))
+    break
+  except ValueError:
+    print("Invalid input!")
+
+while True: 
+  try:
+    height = float(input("Enter your height in meters (between 0-3) "))
+    break
+  except ValueError:
+    print("Invalid input!")
 # Processing
 
 # Output
-print("You weigh " + weight + "kgs" + " and your height is " + height + " meters!")
+print("You weigh " + str(weight) + "kgs" + " and your height is " + str(height) + " meters!")
+
