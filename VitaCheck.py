bf2bb017a7e4bb04d5693b4e43b3e15292e9cb5d
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2026-07-08 12:46:36 +0100

    Edited the input and output message

--- VitaCheck.py
+++ VitaCheck.py
@@ -1,8 +1,9 @@
 # Input
-print("Enter your weight:")
+print("Enter your weight in kilograms (0-200):")
 weight = input()
-print("Enter your height:")
+print("Enter your height in meters (0 - 3):")
 height = input()
 # Processing
+
 # Output
-print("You weigh: " + weight + " and you are " + height + " meters tall")
+print("You weigh " + weight + "kgs" + " and your height is " + height + " meters!")
